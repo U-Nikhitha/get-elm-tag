30220c219c29ab63a5d1b54ab215ce528eddb45a
U-Nikhitha <uthoornikitha75785@gmail.com>
2023-04-17 16:30:00 +0530

    get element by tag name

---
+++ dom.js
@@ -0,0 +1,7 @@
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[2]);
+li[2].style.backgroundColor='green';
+for (var i=0; i < li.length; i++){
+    li[i].style.fontWeight='bold';
+}
